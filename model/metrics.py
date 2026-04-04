9c44199b579d5b4f8401b04920e54f30bc1ca479
minhman293 <mantruong293@gmail.com>
2026-04-04 13:03:42 +0800

    modify linear regression

--- model/metrics.py
+++ model/metrics.py
@@ -33,12 +33,17 @@ def evaluate_linear_regression(y_true, y_pred, title='Linear Regression Evaluati
     y_true = np.asarray(y_true).ravel()
     y_pred = np.asarray(y_pred).ravel()
 
-    # TODO 
+    # Compute metrics using sklearn
+    mse = mean_squared_error(y_true, y_pred)
+    mae = mean_absolute_error(y_true, y_pred)
+    rmse = np.sqrt(mse)
+    r2 = r2_score(y_true, y_pred)
+    
     metrics = {
-        'MSE': 'TODO: use sklearn.metrics to compute MSE',
-        'MAE': 'TODO: use sklearn.metrics to compute MAE',
-        'RMSE': 'TODO: use sklearn.metrics and numpy to compute RMSE',
-        'R-squared': 'TODO: use sklearn.metrics to compute R-squared',
+        'MSE': mse,
+        'MAE': mae,
+        'RMSE': rmse,
+        'R-squared': r2,
     }
 
     print(f"=== {title} ===")
